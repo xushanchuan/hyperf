e70cc3cebfd7d1869a2f55b7bfdcffcc93cd6f13
徐山川 <635512931@qq.com>
2021-02-24 11:08:51 +0800

    1

--- hyperf-skeleton/app/Controller/UserController.php
+++ hyperf-skeleton/app/Controller/UserController.php
@@ -10,29 +10,29 @@ use Hyperf\Logger\LoggerFactory;
 
 class UserController extends AbstractController
 {
-    private $service;
-
-    /**
-     * @var \Psr\Log\LoggerInterface
-     */
-    protected $logger;
-
-    public function __construct(UserService $service,LoggerFactory $loggerFactory)
-    {
-        $this->service = $service;
-        $this->logger = $loggerFactory->get('log', 'default');
-    }
-
-    public function getUserById(int $userId)
-    {
-        $user = $this->service->getById($userId);
-        return $user;
-    }
-
-    public function test()
-    {
-        return User::query()->first()->toArray();
-    }
+//    private $service;
+//
+//    /**
+//     * @var \Psr\Log\LoggerInterface
+//     */
+//    protected $logger;
+//
+//    public function __construct(UserService $service,LoggerFactory $loggerFactory)
+//    {
+//        $this->service = $service;
+//        $this->logger = $loggerFactory->get('log', 'default');
+//    }
+//
+//    public function getUserById(int $userId)
+//    {
+//        $user = $this->service->getById($userId);
+//        return $user;
+//    }
+//
+//    public function test()
+//    {
+//        return User::query()->first()->toArray();
+//    }
 
     public function test2()
     {
